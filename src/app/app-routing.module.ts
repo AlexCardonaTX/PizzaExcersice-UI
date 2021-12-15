1285490ecb3d89a2ee99ee3ad020a9d144c724a5
Alex Cardona <alex.cardona@truextend.com>
2021-12-15 08:35:39 -0400

    Installed Bootstrap and generated toppings component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToppingsComponent } from './toppings/toppings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "/toppings", component: ToppingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
